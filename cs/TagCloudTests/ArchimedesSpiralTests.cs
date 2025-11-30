895c81fe79fc45676a4681572149e9de759724a9
Тимур Бабаев <truefoldergames@gmail.com>
2025-11-30 05:35:06 +0500

    Разделил тесты ArchimedesSpiral на два разных

--- cs/TagCloudTests/ArchimedesSpiralTests.cs
+++ cs/TagCloudTests/ArchimedesSpiralTests.cs
@@ -14,9 +14,19 @@ public class ArchimedesSpiralTests
         var center = new PointF(0, 0);
         archimedesSpiral = new ArchimedesSpiral(center, 1, 1);
         
-        var point = archimedesSpiral.GetNextPoint().Take(2).ToList();
+        var point = archimedesSpiral.GetNextPoint().Take(1).ToList();
 
         point.First().Should().Be(center);
+    }
+
+    [Test]
+    public void GetNextPoint_TwoPointsShouldNotBeSame()
+    {
+        var center = new PointF(0, 0);
+        archimedesSpiral = new ArchimedesSpiral(center, 1, 1);
+        
+        var point = archimedesSpiral.GetNextPoint().Take(2).ToList();
+        
         point.Last().Should().NotBe(point.First());
     }
 }
